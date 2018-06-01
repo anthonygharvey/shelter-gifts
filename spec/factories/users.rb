688384e0631b88cfbdb8c0f54e28f60fde721e79
anthonygharvey <anthony@anthonygharvey.com>
2018-06-01 11:30:57 -0400

    generate User model

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+require 'faker'
+FactoryBot.define do
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    username { Faker::Internet.username }
+    email { Faker::Internet.email }
+    password "password"
+  end
+end
