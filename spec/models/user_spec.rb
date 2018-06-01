2e039873798c6e141f87e15a3a100ba3ae50adfd
anthonygharvey <anthony@anthonygharvey.com>
2018-06-01 11:57:21 -0400

    Add user factory test.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "User Factory" do
+    it "has a valid user factory with a first_name, last_name, username, email and password" do
+      expect(build(:user)).to be_valid
+    end
+  end
+  
 end
