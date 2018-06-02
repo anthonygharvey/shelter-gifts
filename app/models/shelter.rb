bddf8d2eba982834fb3b484459d3f4183f25c7fa
anthonygharvey <anthony@anthonygharvey.com>
2018-06-01 20:07:37 -0400

    Add list factory test

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,5 +1,6 @@
 class Shelter < ApplicationRecord
 	belongs_to :user
+	has_many :lists
 	validates :name, :city, :state, :ein, :shelter_type, presence: true
 	validates_format_of :ein, with: /\A^[1-9]\d?-\d{7}$\z|\A\d{9}\z/
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before(:each) do
+    @user = build(:user)
+    @shelter = build(:shelter, user: @user)
+  end
+
+  describe "List Factory" do
+    it "has a valid list factory with a name, description and url" do
+      expect(build(:list, shelter: @shelter)).to be_valid
+    end
+  end
+
+
 end
