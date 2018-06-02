8b96c1c68ac28db46bb5637e76aa202f1e95c586
anthonygharvey <anthony@anthonygharvey.com>
2018-06-01 22:43:52 -0400

    Add pending item tests and price validation test

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
 	belongs_to :list
-	
+	validates :name, :price, presence: true
+
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,4 +12,31 @@ RSpec.describe Item, type: :model do
       expect(build(:item, list: @list)).to be_valid
     end
   end
+
+  describe "Item Validations" do
+    it "is invalid without a name" do
+      expect(build(:item, list: @list, name: nil)).not_to be_valid
+    end
+
+    it "is invalide without a price" do
+      expect(build(:item, list: @list, price: nil)).not_to be_valid
+    end
+
+    it "is invalid without a photo"
+
+    it "is invalid without a prime_status"
+
+    it "is valid without a comment"
+
+    it "is invalid without a quantity"
+
+    it "is valid without a priority"
+
+    it "is invalid without a has_amount"
+
+    it "is invalid without a URL"
+
+    
+  end
+
 end
