462dbc20d2c7f88e464348e9daa76e6552a2ef43
anthonygharvey <anthony@anthonygharvey.com>
2018-06-05 23:15:33 -0400

    Add Shelters controller and stubed actions.

---
+++ app/controllers/shelters_controller.rb
@@ -0,0 +1,24 @@
+class SheltersController < ApplicationController
+	def index
+	end
+
+	def new
+		@shelter = Shelter.new
+	end
+
+	def create
+
+	end
+	
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+end
