283fd042ab97ee217d5d0d4e62a19728ad6408a2
anthonygharvey <anthony@anthonygharvey.com>
2018-06-06 21:36:31 -0400

    Add List #new, #create & #show actions. List new and show views.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,37 @@
 class ListsController < ApplicationController
+	def index
+	end
+
+	def new
+		@shelter = Shelter.find(params[:shelter_id])
+		@list = List.new
+	end
+
+	def create
+		@shelter = Shelter.find(params[:shelter_id])
+		@list = @shelter.lists.build(list_params)
+		if @list.save
+			redirect_to list_path(@list)
+		else
+			render "new"
+		end
+	end
+
+	def show
+		@list = List.find(params[:id])
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+	def list_params
+		params.require(:list).permit(:name, :description, :url)
+	end
 end
