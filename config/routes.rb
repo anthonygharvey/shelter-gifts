8b7e7cc3c87c090335096fc4b3d5151c337a2e7a
anthonygharvey <anthony@anthonygharvey.com>
2018-06-06 20:52:59 -0400

    Add Sessions controller and actions.  Add sessions new view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get '/login' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
   resources :users do
     resources :shelters, shallow: true do
       resources :lists, shallow: true
     end
   end
+
+  # root 'shelters#show'
+
 end
