691f256e789ddddf728f38bad1f6b61ce290bcca
anthonygharvey <anthony@anthonygharvey.com>
2018-06-16 12:24:23 -0400

    update environment file

--- config/environment.rb
+++ config/environment.rb
@@ -2,9 +2,5 @@
 require_relative 'application'
 require 'open-uri'
 
-configure :production do
-  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-end
-
 # Initialize the Rails application.
 Rails.application.initialize!
