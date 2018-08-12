2095cd333ed30dbfa2e52fbe27755b8df5c2f694
anthonygharvey <anthony@anthonygharvey.com>
2018-08-12 17:01:53 -0400

    Remove show action exception for authentication_required

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
-	before_action :authentication_required, except: [:new, :create, :index, :show]
+	before_action :authentication_required, except: [:new, :create, :index]
 
 	def authentication_required
 		if !logged_in?
