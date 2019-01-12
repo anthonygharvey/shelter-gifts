d1ee82600a73915ce4cf81511cc39ef298e252f3
anthonygharvey <anthony@anthonygharvey.com>
2019-01-12 01:26:16 -0500

    Refactored ItemsController, added id to search form
    Added json format rendering
    Added search params for filtering
    Refactored search filtering
    Added search form id

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,33 +1,33 @@
 class ItemsController < ApplicationController
 	def index
-		search = [params["city"], params["state"], params["shelter_type"], params["priority"]]
+		@all_items = search_items || Item.all
+		respond_to do |format|
+			format.html {render layout: true}
+			format.json { render json: @all_items, include: [:list, "list.shelter"], status: 200 }
+		end
+	end
+
+	private def search_items
+		search = [search_params["city"], search_params["state"], search_params["shelter_type"], search_params["priority"]]
+
 		if search.all?{|s| s == nil}
 			@all_items = Item.all
 		else
 			@shelters = Shelter.all
-				if params["city"]
-					@shelters = @shelters.by_city(params["city"])
-				end
-				if params["state"]
-					@shelters = @shelters.by_state(params["state"])
-				end
-				if params["shelter_type"]
-					@shelters = @shelters.by_type(params["shelter_type"])
-				end
+			@shelters = @shelters.by_city(params["city"]) if params["city"]
+			@shelters = @shelters.by_state(params["state"]) if params["state"]
+			@shelters = @shelters.by_type(params["shelter_type"]) if params["shelter_type"]
 			@all_items = @shelters.collect do |s|
 				if (params["priority"] != "" && params["priority"] != nil)
-					s.items.select do |i|
-						i.priority == params["priority"].downcase
-					end
+					s.items.select { |i| i.priority == params["priority"].downcase }
 				else
 					s.items
 				end
 			end.flatten
 		end
-		respond_to do |format|
-			format.html { render :index }
-			format.json { render json: @all_items, include: [:list, "list.shelter"], status: 200 }
-			format.js { }
-		end
+	end
+
+	private def search_params
+		params.permit(:city, :state, :shelter_type, :priority)
 	end
 end
