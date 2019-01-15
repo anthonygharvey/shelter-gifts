af5e8a87e3b25e76ea03c67e1ea4e63228b932d5
anthonygharvey <anthony@anthonygharvey.com>
2019-01-14 20:37:07 -0500

    Implemented server-side ajax for homepage search

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,9 +1,17 @@
-$(function(){
-	$.ajax({
-		url: this.href,
-		dataType: 'script'
-	})
-
-
+$(function () {
+	$("form input#search").on("click", function (e) {
+		$.ajax({
+			method: 'GET',
+			url: this.parentElement.action,
+			data: {
+				city: $('#city')[0].value,
+				state: $('#state')[0].value,
+				shelter_type: $('#type')[0].value,
+				priority: $('#priority')[0].value
+			},
+			dataType: "script"
+		})
+		e.preventDefault();
+	});
 })
 
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,25 +1,20 @@
 class ItemsController < ApplicationController
 	def index
 		@all_items = search_items || Item.all
-		respond_to do |format|
-			format.html {render layout: true}
-			format.json { render json: @all_items, include: [:list, "list.shelter"], status: 200 }
-		end
 	end
 
 	private def search_items
-		search = [search_params["city"], search_params["state"], search_params["shelter_type"], search_params["priority"]]
-
-		if search.all?{|s| s == nil}
+		search = [params[:city], params[:state], params[:type], params[:priority]]
+		test = if search.all?{|s| s == nil}
 			@all_items = Item.all
 		else
 			@shelters = Shelter.all
-			@shelters = @shelters.by_city(params["city"]) if params["city"]
-			@shelters = @shelters.by_state(params["state"]) if params["state"]
-			@shelters = @shelters.by_type(params["shelter_type"]) if params["shelter_type"]
+			@shelters = @shelters.by_city(params[:city])
+			@shelters = @shelters.by_state(params[:state])
+			@shelters = @shelters.by_type(params[:shelter_type])
 			@all_items = @shelters.collect do |s|
-				if (params["priority"] != "" && params["priority"] != nil)
-					s.items.select { |i| i.priority == params["priority"].downcase }
+				if (params[:priority] != "" && params[:priority] != nil)
+					s.items.select { |i| i.priority == params[:priority].downcase }
 				else
 					s.items
 				end
