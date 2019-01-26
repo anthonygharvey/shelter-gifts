0ccda4e241612e9969b5f3ca87c9ef434e73ae7f
anthonygharvey <anthony@anthonygharvey.com>
2019-01-26 02:12:23 -0500

    Removed omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,8 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		# Check for omniauth
-		if auth = request.env["omniauth.auth"]
-			user = User.find_or_create_by(uid: auth['uid']) do |u|
-				u.first_name, u.last_name = auth['info']['name'].split(' ')
-				if auth['info']['email'] == nil
-					u.email = SecureRandom.base64(25) + "@noemail.com"
-				else
-					u.email = auth['info']['email']
-				end
-				u.password = SecureRandom.base64(25)
-				u.username = SecureRandom.base64(25)
-			end
+		user = User.find_by(:email => params[:email])
+		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
 			if user.shelter == nil
 				redirect_to new_user_shelter_path(user)
@@ -22,18 +12,7 @@ class SessionsController < ApplicationController
 				redirect_to shelter_path(user.shelter)
 			end
 		else
-		# If not omniauth, regular user login
-			user = User.find_by(:email => params[:email])
-			if user && user.authenticate(params[:password])
-				session[:user_id] = user.id
-				if user.shelter == nil
-					redirect_to new_user_shelter_path(user)
-				else
-					redirect_to shelter_path(user.shelter)
-				end
-			else
-				render "sessions/new"
-			end
+			render "sessions/new"
 		end
 	end
 
