1bff731660b8eb1e40609f617da923f4374116c5
anthonygharvey <anthony@anthonygharvey.com>
2019-01-25 21:01:43 -0500

    Refactored items_controller to remove redundant code

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,19 @@
 class ItemsController < ApplicationController
 	def index
-		@all_items = search_items || Item.all
-		render json: @all_items
+		render json: @all_items = search_items || Item.all
 	end
 
 	private def search_items
 		search = [params[:city], params[:state], params[:type], params[:priority]]
-		test = if search.all?{|s| s == nil}
-			@all_items = Item.all
+		if search.all?{|s| s == nil}
+			Item.all
 		else
 			@shelters = Shelter.all
 				.by_city(params[:city])
 				.by_state(params[:state])
 				.by_type(params[:shelter_type])
 				
-			@all_items = @shelters.collect do |s|
+			@shelters.collect do |s|
 				if (params[:priority] != "" && params[:priority] != nil)
 					s.items.select { |i| i.priority == params[:priority].downcase }
 				else
